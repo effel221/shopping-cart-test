ff9c8ee1cc40e00841c8f11e99b34197a49dc6a3
Tatsiana-Salikayeva <tatsiana.salikayeva@promaterial.com>
2024-09-14 12:45:35 +0200

    add new components

---
+++ src/app/Components/Main.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Main() {
+  return (
+     <main class="container mx-auto px-4">
+         <h1>TS shopping cart solution</h1>
+     </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/app/Components/Header";
+import Main from "@/app/Components/Main";
 
 
 export default function Home() {
   return (
      <>
          <Header/>
-        ak;ldka;ldsk
+         <Main/>
      </>
   );
 }
