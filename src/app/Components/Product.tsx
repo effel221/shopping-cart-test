9a2c5e3dfef381ae9c221a0287769c923b3724e1
Tatsiana-Salikayeva <tatsiana.salikayeva@promaterial.com>
2024-09-14 19:54:51 +0200

    add add to cart button

--- src/app/Components/Product.tsx
+++ src/app/Components/Product.tsx
@@ -4,13 +4,14 @@ import {ProductType} from "@/app/types";
 
 
 export default function Product(product:ProductType) {
-  const {name, price, description} = product
+  const {id, name, price, description} = product
   return (
-     <div className={`${styles.product} flex basis-1 md:basis-1/3 lg:basis-1/4`} aria-labelledby="productName">
+     <div className={`${styles.product} flex w-full md:w-1/3 lg:w-1/4`} aria-labelledby={id}>
          <div className={styles.productDetails}>
-             <h2 id="productName">{name}</h2>
+             <h2 id={id}>{name}</h2>
              <p className={styles.price}>{price} €</p>
              <p className={styles.description}>{description}</p>
+             <button className="flex ml-auto">Add to Cart</button>
          </div>
      </div>
   );
